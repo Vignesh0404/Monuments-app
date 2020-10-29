9645d5cf4533be4efb81543f8c942f428a6175bf
swaran18999 <swaran18999@gmail.com>
2020-10-29 13:36:21 +0530

    Login using BLoC

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:travelapp/screens/login.dart';
-import 'package:travelapp/screens/signup.dart';
-import 'package:travelapp/screens/welcome.dart';
-import 'package:travelapp/widgets/bottomNavigation.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:travelapp/app.dart';
+import 'package:travelapp/simple_bloc_observer.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BottomNavigation(),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  EquatableConfig.stringify = kDebugMode;
+  Bloc.observer = SimpleBlocObserver();
+  runApp(App(authenticationRepository: AuthenticationRepository()));
 }
